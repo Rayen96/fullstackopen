fbbd19fd887c6f9c42196e212bec2ed1c32901d9
Rayen <rayentan96@gmail.com>
2019-06-26 10:35:22 +0800

    added heroku backend

---
+++ part3/phonebook backend/index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const cors = require('cors');
+app.use(cors());
+
+
+app.use(bodyParser.json());
+
+morgan.token('json', (req, res) =>
+ JSON.stringify(req.body))
+
+
+app.use(morgan('tiny', {skip: (req, res) => req.method === 'POST'}))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json',
+  {skip: (req, res) => req.method !== 'POST'}));
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.get('/info', (req, res) => {
+  res.send(
+    `<div>Phonebook has info for ${persons.length} people</div>
+    <div>${new Date()}</div>`
+  )
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  res.status(204).end()
+});
+
+app.post('/api/persons', (req, res) => {
+  const maxId = 1000;
+  const id = Math.floor(Math.random() * maxId);
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'name missing',
+    });
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing',
+    });
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name already exists',
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: id,
+  }
+  
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
